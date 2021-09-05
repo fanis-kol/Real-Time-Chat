8aa9af1a95f843be26559555958b1f869ce04f7b
Fanis <fanis.kolokythas@gmail.com>
2021-09-05 21:33:58 +0300

    chat

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-
-const app = express();
+const express = require('express')
+const app = express()
 const server = require('http').createServer(app)
 const port = process.env.PORT || 3000
+const io = require('socket.io')(server)
+const path = require('path')
 
-app.get('/',(req, res)=> {
-    res.status(200).send("working");
+app.use(express.static(path.join(__dirname + '/public')))
+
+io.on('connection', socket => {
+  console.log('Some client connected')
 })
 
-server.listen(port, ()=> {
-    console.log(`server is running on port ${port}`)
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
 })
